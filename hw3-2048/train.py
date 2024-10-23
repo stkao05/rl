cc7032c2b6bdf9576e5c0e9cf7654c591cf4ba3e
Steven Kao <st.kao.05@gmail.com>
2024-10-23 15:12:17 +0800

    refactor train code: better print. wandb

--- hw3-2048/train.py
+++ hw3-2048/train.py
@@ -9,21 +9,14 @@ from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.vec_env import DummyVecEnv, VecVideoRecorder
 from stable_baselines3 import A2C, DQN, PPO, SAC
 
-
 warnings.filterwarnings("ignore")
-register(
-    id='2048-v0',
-    entry_point='envs:My2048Env'
-)
+register(id="2048-v0", entry_point="envs:My2048Env")
 
-# Set hyper params (configurations) for training
 my_config = {
     "run_id": "example",
-
     "algorithm": PPO,
     "policy_network": "MlpPolicy",
     "save_path": "models/sample_model",
-
     "epoch_num": 5,
     "timesteps_per_epoch": 1000,
     "eval_episode_num": 10,
@@ -32,8 +25,9 @@ my_config = {
 
 
 def make_env():
-    env = gym.make('2048-v0')
+    env = gym.make("2048-v0")
     return env
+
 
 def eval(env, model, eval_episode_num):
     """Evaluate the model and return avg_score and avg_highest"""
@@ -41,85 +35,65 @@ def eval(env, model, eval_episode_num):
     avg_highest = 0
     for seed in range(eval_episode_num):
         done = False
-        # Set seed using old Gym API
-        env.seed(seed)
+        env.seed(seed) # set seed using old Gym API
         obs = env.reset()
 
-        # Interact with env using old Gym API
         while not done:
             action, _state = model.predict(obs, deterministic=True)
             obs, reward, done, info = env.step(action)
-        
-        avg_highest += info[0]['highest']
-        avg_score   += info[0]['score']
+
+        avg_highest += info[0]["highest"]
+        avg_score += info[0]["score"]
 
     avg_highest /= eval_episode_num
     avg_score /= eval_episode_num
-        
+
     return avg_score, avg_highest
+
 
 def train(eval_env, model, config):
     """Train agent using SB3 algorithm and my_config"""
     current_best = 0
     for epoch in range(config["epoch_num"]):
-
-        # Uncomment to enable wandb logging
         model.learn(
             total_timesteps=config["timesteps_per_epoch"],
             reset_num_timesteps=False,
-            # callback=WandbCallback(
-            #     gradient_save_freq=100,
-            #     verbose=2,
-            # ),
+            callback=WandbCallback(
+                gradient_save_freq=100,
+                verbose=2,
+            ),
         )
 
-        ### Evaluation
-        # print(config["run_id"])
-        # print("Epoch: ", epoch)
         avg_score, avg_highest = eval(eval_env, model, config["eval_episode_num"])
-        
-        # print("Avg_score:  ", avg_score)
-        # print("Avg_highest:", avg_highest)
-        # print()
-        # wandb.log(
-        #     {"avg_highest": avg_highest,
-        #      "avg_score": avg_score}
-        # )
-        
+        is_better = current_best < avg_score
 
-        ### Save best model
-        if current_best < avg_score:
-            print("Saving Model")
+        print(f"epoch: {epoch} | avg score: {avg_score} | avg highest: {avg_highest} | best: {is_better}")
+        wandb.log({"avg_highest": avg_highest, "avg_score": avg_score})
+
+        if is_better:
             current_best = avg_score
             save_path = config["save_path"]
-            # model.save(f"{save_path}/{epoch}")
-
-        # print("---------------")
+            model.save(f"{save_path}/{epoch}")
 
 
 if __name__ == "__main__":
+    run = wandb.init(
+        project="rl-2048",
+        config=my_config,
+        sync_tensorboard=True,  # auto-upload sb3's tensorboard metrics
+        id=my_config["run_id"],
+    )
 
-    # Create wandb session (Uncomment to enable wandb logging)
-    # run = wandb.init(
-    #     project="assignment_3",
-    #     config=my_config,
-    #     sync_tensorboard=True,  # auto-upload sb3's tensorboard metrics
-    #     id=my_config["run_id"]
-    # )
-
-    # Create training environment 
     num_train_envs = 2
     train_env = DummyVecEnv([make_env for _ in range(num_train_envs)])
-
-    # Create evaluation environment 
-    eval_env = DummyVecEnv([make_env])  
+    eval_env = DummyVecEnv([make_env])
 
     # Create model from loaded config and train
     # Note: Set verbose to 0 if you don't want info messages
     model = my_config["algorithm"](
-        my_config["policy_network"], 
-        train_env, 
-        verbose=2,
+        my_config["policy_network"],
+        train_env,
+        verbose=0,
         tensorboard_log=my_config["run_id"],
         learning_rate=my_config["learning_rate"],
     )
